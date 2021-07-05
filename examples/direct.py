32a7c30f5a829b12d3f948fd59bf4b1e93930b55
Isaac K <65869106+isaackogan@users.noreply.github.com>
2021-07-05 00:17:40 -0400

    Update direct.py

--- examples/direct.py
+++ examples/direct.py
@@ -1,8 +1,17 @@
 from OmeglePy import OmegleClient, EventHandler
 
-client = OmegleClient(EventHandler(), topics=['tiktok'], debug=True)
+client = OmegleClient(EventHandler(), topics=['tiktok'], debug=False)
 client.start()
 
 while True:
+
+    # Get input
     message = input()
+    
+    # Go to the next person
+    if message.lower() == "/next":
+        client.loop.creatae_task(client.skip())
+        continue
+    
+    # Send a message
     client.loop.create_task(client.send(message))
